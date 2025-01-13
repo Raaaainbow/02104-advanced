7368dedfb67714b90fbb66db456f58ee62e6e4ae
slayervictor <victor.r2002@gmail.com>
2025-01-13 12:55:52 +0100

    savestart

--- breakout/src/main/java/com/example/App.java
+++ breakout/src/main/java/com/example/App.java
@@ -90,6 +90,22 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+    public static void save(String keyword, Number value) {
+        
+    }
+
+    public static void save(String keyword, String value) {
+
+    }
+
+    public static Number loadNumber(String keyword) {
+        return 69.09;
+    }
+
+    public static String loadString(String keyword) {
+        return "L";
+    }
+
     public static void main(String[] args) { // Launching the game
         launch(args);
     }
